445709bd2d1f874d7702310bce9b4d40e8af2bf4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+
+namespace SchedulerXPOWpf {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+
+            XpoDefault.DataLayer = XpoDefault.GetDataLayer(
+                AccessConnectionProvider.GetConnectionString("SchedulerXPOWpf.mdb"),
+                //MSSqlConnectionProvider.GetConnectionString(".\\SQLExpress", "SchedulerXPOWpf"),
+                AutoCreateOption.DatabaseAndSchema);
+        }
+    }
+}
